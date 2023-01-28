da81e592f5ea8ae909697cc3cddb7375fd884745
GamerProg34 <shokugekikibou@gmail.com>
2023-01-28 04:18:30 -0500

    Inventory Scripts
    -finished most of the scripts that will be associated with the inventory system

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Sword,
+        HealthPotion,
+        ManaPotion,
+        Coin,
+        Medkit,
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+    public Sprite GetSprite()
+    {
+        switch (itemType)
+        {
+            default:
+            case   ItemType.Sword:         return ItemAssets.Instance.swordSprite;
+            case   ItemType.HealthPotion:  return ItemAssets.Instance.healthPotionSprite;
+            case   ItemType.ManaPotion:    return ItemAssets.Instance.manaPotionSprite;
+            case   ItemType.Coin:          return ItemAssets.Instance.coinSprite;
+            case   ItemType.Medkit:        return ItemAssets.Instance.medkitSprite;
+        }
+    }
+
+    public bool IsStackable()
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.Coin:
+            case ItemType.HealthPotion:
+            case ItemType.ManaPotion:
+                return true;
+            case ItemType.Sword:
+            case ItemType.Medkit:
+                return false;
+        }
+    }
+}
