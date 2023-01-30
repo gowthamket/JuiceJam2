15285389f7f9338aa84eb97b9acdbdb233ef431a
GamerProg34 <shokugekikibou@gmail.com>
2023-01-29 19:33:33 -0500

    UI Character Portrait
    -got started on scripts that will create UI image of the player for the sake of equipping weapons and/or armor

--- Assets/Scripts/GameHandler/GameHandler.cs
+++ Assets/Scripts/GameHandler/GameHandler.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameHandler instance;
+    public Canvas canvas;
+    public Transform pfWindow_CharacterPortrait;
+
+    //[SerializeField] private CameraFollow cameraFollow;
+    private Vector3 cameraFollowPosition = new Vector3(-134, 150);
+
+    [SerializeField] private Transform characterTransform;
+    [SerializeField] private Transform secondCharacterTransform;
+
+    private Player rifleCharacter;
+    private Player swordCharacter;
+
+    private void Awake()
     {
-        
+        instance = this;
+        //cameraFollow.Setup(() => cameraFollowPosition, () => 80f);
+        SetupCharacters();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //characterTransform.GetComponent<Button>().onClick.AddListener(Window_CharacterPortrait.Show_Static(characterTransform));
+
+        //secondCharacterTransform.GetComponent<Button>().onClick.AddListener(Window_CharacterPortrait.Show_Static(secondCharacterTransform));
+    }
+
+    private void Update()
+    {
+        HandleCameraMovement();
+    }
+
+    private void SetupCharacters()
+    {
+
+    }
+
+    private void HandleCameraMovement()
+    {
+
     }
 }
