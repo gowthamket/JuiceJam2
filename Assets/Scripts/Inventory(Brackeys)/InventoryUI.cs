1bec4706236190f0937b1e3dd7a31c6fa886b632
GamerProg34 <shokugekikibou@gmail.com>
2023-02-02 13:20:03 -0500

    Reworked Inventory
    -just need to introduce the ability to combine items to form weapons now

--- Assets/Scripts/Inventory(Brackeys)/InventoryUI.cs
+++ Assets/Scripts/Inventory(Brackeys)/InventoryUI.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform itemsParent;
+
+    Inventory inventory;
+
+    InventorySlot[] slots;
+
+    public GameObject inventoryUI;
+
     void Start()
     {
-        
+        inventory = Inventory.Instance;
+        inventory.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryUI.SetActive(inventoryUI.activeSelf);
+        }
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,79 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using CodeMonkey.Utils;
 
 public class Player : MonoBehaviour
 {
-    public static Player Instance { get; private set; }
+    
 
-    private const float SPEED = 50f;
-
-    //[SerializeField] private MaterialTintColor materialTintColor;
-
-    //private Player_Base player;
-    //private State state;
-
-    public event EventHandler OnGoldAmountChanged;
-    public event EventHandler OnHealthPotionAmountChanged;
-    public event EventHandler OnEquipChanged;
-
-    private Inventory inventory;
-
-    //[SerializeField] private MaterialTintColor
-    [SerializeField] private UI_Inventory uiInventory;
-
-    private void Awake()
-    {
-        Instance = this;
-
-        inventory = new Inventory();
-        uiInventory.SetInventory(inventory);
-
-        ItemWorld.SpawnItemWorld(new Vector3(20, 20), new Item { itemType = Item.ItemType.HealthPotion, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(-20, 20), new Item { itemType = Item.ItemType.ManaPotion, amount = 1 });
-        ItemWorld.SpawnItemWorld(new Vector3(0, -20), new Item { itemType = Item.ItemType.Sword, amount = 1 });
-    }
-
-    public void UseItem(Item inventoryItem)
-    {
-        Debug.Log("Use Item: " + inventoryItem);
-    }
-
-    public Inventory GetInventory()
-    {
-        return inventory;
-    }
-
-    public void SetEquipment(Item item)
-    {
-        SetEquipment(item.itemType);
-    }
-
-    public void SetEquipment(Item.ItemType itemType)
-    {
-        // Equip Item
-        switch (itemType)
-        {
-            //case Item.ItemType.ArmorNone: EquipArmorNone(); break;
-            //case Item.ItemType.Armor_1: EquipArmor_1(); break;
-            //case Item.ItemType.Armor_2: EquipArmor_2(); break;
-
-            //case Item.ItemType.HelmetNone: EquipHelmetNone(); break;
-            //case Item.ItemType.Helmet: EquipHelmet(); break;
-
-            //case Item.ItemType.SwordNone: EquipWeapon_Punch(); break;
-            //case Item.ItemType.Sword_1: EquipWeapon_Sword(); break;
-            //case Item.ItemType.Sword_2: EquipWeapon_Sword2(); break;
-        }
-        OnEquipChanged?.Invoke(this, EventArgs.Empty);
-    }
+    Camera cam;
+    public Interactable focus;
 
     private void Update()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
         if (Input.GetMouseButtonDown(1))
         {
-            
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        focus = newFocus;
+    }
+
+    void RemoveFocus()
+    {
+        focus = null;
     }
 }
